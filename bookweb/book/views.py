61a8ea4e3dd518d11a355962927446c98a62d8e2
Anjali <amarak@o2.pl>
2019-09-10 16:18:41 +0200

    templates

--- bookweb/book/views.py
+++ bookweb/book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Book
 # Create your views here.
+
+class BookList(ListView):
+    model = Book
+
+
+class BookDetail(DetailView):
+    model = Book
