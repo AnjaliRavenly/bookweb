cb508ec595f3b255f15cf41e27a4b6d30c5717dc
Anjali <amarak@o2.pl>
2019-09-12 00:54:46 +0200

    book details

--- bookweb/book/models.py
+++ bookweb/book/models.py
@@ -4,12 +4,12 @@ from django.utils.text import slugify
 # Create your models here.
 
 CATEGORY_BOOKS = {
-    ("fantasty", "FANTASY"),
+    ("fantasy", "FANTASY"),
     ("sci_fi", "SCI_FI"),
     ("horror", "HORROR"),
     ("biographies", "BIOGRAPHIES"),
     ("romance", "ROMANCE"),
-    ("thrillers", "THRILLERS"),
+    ("thrillers", "THRILLER"),
 }
 
 class Book(models.Model):
@@ -21,7 +21,7 @@ class Book(models.Model):
     category = category = models.CharField(choices=CATEGORY_BOOKS, max_length=15)
     description = models.TextField(max_length=1000)
     main_characters = models.CharField(max_length=200)
-    name_series = models.CharField(max_length=100, blank=True)
+    series = models.CharField(max_length=100, blank=True)
     film_adaptation = models.BooleanField(default=False)
     movie_trailer = models.URLField(blank=True)
     tags = models.CharField(max_length=100)
@@ -34,18 +34,7 @@ class Book(models.Model):
             self.slug = slugify(self.title)
         super(Book, self).save(*args, **kwargs)
 
+
     def __str__(self):
         return self.title+ " - " + str(self.author)
 
-"""- Tytuł
--autor
-- data wydania
-- zdjęcie okładki
-- kategoria
-- opis
--bohaterowie
-- cykl
--adaptacja filmowa 
-- link do trailera
-- tags
-"""
--- bookweb/book/views.py
+++ bookweb/book/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Book
+from django.db.models import Q
 # Create your views here.
 
 class BookList(ListView):
     model = Book
-
+    paginate_by = 5
 
 class BookDetail(DetailView):
     model = Book
+    # def get_object(self, queryset=None):
+    #     object = super(BookDetail, self).get_object()
+    #     object.views_count += 1
+    #     object.save()
+    #     return object
+
+
+    def get_context_data(self, **kwargs):
+        context = super(BookDetail, self).get_context_data(**kwargs)
+        context['book_series'] = Book.objects.filter(series=self.get_object().series)
+        context['book_category'] = Book.objects.filter(category=self.get_object().category).filter(~Q(series=self.get_object().series))
+        return context
+
+
+class BookCategory(ListView):
+    model = Book
+
+    def get_object(self, queryset=None):
+        pass
+
+    def get_context_data(self, **kwargs):
+        context = super(BookDetail, self).get_context_data(**kwargs)
+        context['book_category'] = Book.objects.filter(category=self.get_object().category)
+        return context
